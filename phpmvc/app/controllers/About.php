2944a3064fa06310afab60f97f46132cec79de40
Muhamad Deni Akbar Alafalah <71306381+MuhamadDeni12@users.noreply.github.com>
2020-10-03 07:51:02 +0700

    Add files via upload

---
+++ phpmvc/app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php 
+
+class About {
+	public function index($nama = 'Deni', $pekerjaan = 'Streamer')
+	{
+		echo "Halo, nama saya $nama, saya adalah Seorang $pekerjaan";
+	}
+	public function page()
+	{
+		echo 'About/page';
+	}
+}
+ ?>}
