9814139349808f92b4a1588ff5e43f737a2e4d23
Muhamad Deni Akbar Alafalah <71306381+MuhamadDeni12@users.noreply.github.com>
2020-10-13 07:44:37 +0700

    Add files via upload

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,13 +1,22 @@
-<?php 
+<?php
 
-class About {
-	public function index($nama = 'Deni', $pekerjaan = 'Streamer')
+class About extends Controller {
+	public function index($nama = 'Asep', $pekerjaan ='Mahasiswa', $umur = 20)
 	{
-		echo "Halo, nama saya $nama, saya adalah Seorang $pekerjaan";
+		$data['nama'] = $nama;
+		$data['pekerjaan'] = $pekerjaan;
+		$data['umur'] = $umur;
+		$data['judul'] = 'About Me';
+		$this->view('templates/header', $data);
+		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
+
 	public function page()
 	{
-		echo 'About/page';
+		$data['judul'] 'Pages';
+		$this->view('templates/header', $data);
+		$this->view('about/page');
+		$this->view('templates/footer');
 	}
-}
- ?>}
+} 
